6bdb5cfce70d11125fe9a457c13fe8291404c0d9
Kosymbaev <93342777+Kosymbaev@users.noreply.github.com>
2022-05-11 12:22:10 +0300

    Release class BoucingBalls and add method in class Field

--- src/bsu/rfe/java/group10/lab1/Kosymbaev/var3/Field.java
+++ src/bsu/rfe/java/group10/lab1/Kosymbaev/var3/Field.java
@@ -7,7 +7,10 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class Field extends JPanel {
+    // Флаг приостановленности движения
     private boolean paused;
+    private boolean pausedFast;
+    private boolean pauseBig;
 
     private ArrayList<BoucingBalls> balls = new ArrayList<BoucingBalls>(10);
 
@@ -17,8 +20,29 @@ public class Field extends JPanel {
             repaint();
         }
     });
+
+
     public Field(){
         setBackground(Color.WHITE);
         repaintTimer.start();
+    }
+
+    public synchronized void pauseFast(){
+        pausedFast = true; //вкл паузу быстрых мячей
+    }
+
+    public synchronized void canMove(BoucingBalls ball) throws
+            InterruptedException {
+        if (paused) {
+            // Если режим паузы включен, то поток, зашедший
+            // внутрь данного метода, засыпает
+            wait();
+        }
+        if(pausedFast && ball.getSpeed() > 8){
+            wait();
+        }
+        if(pauseBig && ball.getRadius() > 20){
+            wait();
+        }
     }
 }
